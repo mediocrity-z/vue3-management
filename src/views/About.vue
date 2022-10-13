b48f1a8119b30c6ffc3951cc62437a03071834ce
祝子莘 <1157023204@qq.com>
2022-10-13 15:55:24 +0800

    feat: 测试

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{ count }}</h1>
+    <button @click="handleClick">点我+1</button>
+    <h1>{{ count1 }}</h1>
   </div>
 </template>
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+
+let count = ref(0);
+function handleClick() {
+  count.value++;
+}
+let count1 = computed(() => {
+  return count.value + "0";
+});
+</script>
